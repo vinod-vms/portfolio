20b6a8938d00a706250bb41d4bafed13c1daf98a
vinod.vms <vinod.sebastian@rediffmail.com>
2019-08-28 22:02:28 +0530

    using Bootstrap

--- slideshow.js
+++
@@ -1,1 +0,0 @@
-var slideIndex=1;function plusSlides(e){showDivs(slideIndex+=e)}function showDivs(e){var s,l=document.getElementsByClassName("mypics");for(e>l.length&&(slideIndex=1),e<1&&(slideIndex=l.length),s=0;s<l.length;s++)l[s].style.display="none";l[slideIndex-1].style.display="block"}showDivs(slideIndex);
--- sticky.js
+++ sticky.js
@@ -1,1 +1,30 @@
-window.onscroll=function(){myFunction22()};var mylogo=document.getElementById("mylogo"),sticky=supermain.offsetTop;function myFunction22(){window.pageYOffset>=sticky?mylogo.classList.add("sticky_logo"):mylogo.classList.remove("sticky_logo")}function myFunction33(){var t=document.getElementById("tnav");if("firststate"===t.className){t.classList+="res_nav"}else{t.classList="firststate"}}
+/*
+window.onscroll = function() {
+    stickyNavFunction()
+};
+*/
+window.addEventListener("scroll", stickyNavFunction)
+
+var logo = document.getElementById("logo"),
+//var main_section = document.getElementById("main_section");
+    sticky = main_section.offsetTop;
+
+function stickyNavFunction() {
+    
+   if ( window.pageYOffset >= sticky) {
+    logo.style.backgroundImage = "url('./logo_green2.svg')";
+    logo.style.backgroundSize = "contain";
+    logo.style.height = "66px";
+    logo.style.width = "255px";
+
+   } else {
+    logo.style.backgroundImage = "url('./logo_green.svg')";
+    logo.style.backgroundSize = "contain";
+    logo.style.height = "66px";
+    logo.style.width = "255px";
+   }
+   
+  // window.pageYOffset >= sticky ? logo.classList.add("sticky_logo") : logo.classList.remove("sticky_logo"); 
+
+}
+
