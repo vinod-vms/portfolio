9cf1f41b22a63c84580b0c4f9ab55191be652bb0
vinod.vms <vinod.sebastian@rediffmail.com>
2019-08-29 10:19:27 +0530

    minor style changes

--- sticky.js
+++ sticky.js
@@ -5,6 +5,7 @@ window.onscroll = function() {
 */
 var x = window.matchMedia("(max-width: 768px)");
 var y = window.matchMedia("(max-width: 350px)");
+var z = window.matchMedia("(max-width: 250px)");
 var logo = document.getElementById("logo");
 //var main_section = document.getElementById("main_section");
     sticky = sticky_point.offsetTop + 100 ;
@@ -14,14 +15,19 @@ window.addEventListener("scroll", stickyNavFunction);
 
 
 function navFunction(){
-    if (y.matches) {
+    if (z.matches) {
+        logo.style.backgroundImage = "url('./logo_green.svg')";
+        logo.style.backgroundSize = "contain";
+        logo.style.backgroundRepeat = "no-repeat";
+        logo.style.height = "26px";
+        logo.style.width = "102px";
+    }  else if (y.matches) {
         logo.style.backgroundImage = "url('./logo_green.svg')";
         logo.style.backgroundSize = "contain";
         logo.style.backgroundRepeat = "no-repeat";
         logo.style.height = "34px";
         logo.style.width = "128px";
-    } else if (x.matches)
-    {
+    } else if (x.matches)  {
         logo.style.backgroundImage = "url('./logo_green.svg')";
         logo.style.backgroundSize = "contain";
         logo.style.backgroundRepeat = "no-repeat";
@@ -41,8 +47,22 @@ function navFunction(){
 
 function stickyNavFunction() {
 
-       
-   if (y.matches) {
+    if (z.matches) {
+        if (window.pageYOffset <= sticky) {
+        logo.style.backgroundImage = "url('./logo_green.svg')";
+        logo.style.backgroundSize = "contain";
+        logo.style.backgroundRepeat = "no-repeat";
+        logo.style.height = "26px";
+        logo.style.width = "102px";
+    } else if (window.pageYOffset >= sticky) {
+        logo.style.backgroundImage = "url('./logo_green2.svg')";
+        logo.style.backgroundSize = "contain";
+        logo.style.backgroundRepeat = "no-repeat";
+        logo.style.height = "26px";
+        logo.style.width = "102px"; 
+    }
+    }     
+    else if (y.matches) {
    if (window.pageYOffset <= sticky) {
     logo.style.backgroundImage = "url('./logo_green.svg')";
     logo.style.backgroundSize = "contain";
